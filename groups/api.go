3602c760d1fad8fd08f8b23afa589af233ee7b8c
Aidan <aidan@fixedpointcode.com>
2019-04-07 16:15:26 +0100

    Added comments for groups

--- groups/api.go
+++ groups/api.go
@@ -1,49 +1,52 @@
 package groups
 
 import (
-	"fmt"
 	"github.com/jinzhu/gorm"
 	"github.com/rs/xid"
+	"log"
 	"time"
 )
 
 func CreateGroup(db *gorm.DB, userId uint, name string, desc string) xid.ID {
-	db.AutoMigrate(&Group{}, &Member{})
-	code := xid.New()
-	fmt.Printf("%s\n", code.String())
+	// Create group
+	code := xid.New() // Create unique code
 	timeNow := time.Now()
-	fmt.Print(userId)
-	var find Group
+	var find Group // Check if group has code already
 	db.Where("code = ? AND created_at = ?", code, timeNow).First(&find)
 	if find.GroupId != 0 {
+		log.Println("Group: ", find.GroupId, " already has this code")
 		return xid.ID{}
 	}
+	// Create group
 	db.Create(&Group{Name: name, Desc: desc, Code: code.String(), CreatorId: userId, CreatedAt: timeNow, Members: []Member{{UserId: userId, JoinedAt: timeNow}}})
-	var member Member
+	var member Member // Add creator as member
 	db.Where("user_id = ? AND joined_at = ?", userId, timeNow).First(&member)
 	return code
 }
 
 func JoinGroup(db *gorm.DB, code string, userID uint) error {
+	// Join group
 	var group Group
-	joinDate := time.Now()
+	joinDate := time.Now() // Fid group with code and user does not have creator's UserId
 	db.Where("code = ? AND creator_id <> ?", code, userID).First(&group)
-	if group.GroupId == 0 {
+	if group.GroupId == 0 { // Check if group exists
+		log.Println("Could not find group with this code")
 		return nil
 	}
+	// Create new member of group
 	db.Create(&Member{UserId: 4, JoinedAt: joinDate, GroupId: group.GroupId})
 	return nil
 }
 
 func GetGroups(db *gorm.DB, userID uint) (groups []Group, groupsAnalysis []GroupTopicAnalysis, err error) {
-	fmt.Println("Groups", userID)
-	var membership []Member
+	// Return all group details which user member of.
+	var membership []Member // Find all groups
 	err = db.Where("user_id = ?", userID).Find(&membership).Error
 	if err != nil {
 		return []Group{}, []GroupTopicAnalysis{}, err
 	}
-	for _, chosenGroup := range membership {
-		var groupAnalysis []GroupTopicAnalysis
+	for _, chosenGroup := range membership { // Iterate through all groups user is a member
+		var groupAnalysis []GroupTopicAnalysis // Get analysis for each group
 		db.Where("group_id = ?", chosenGroup.GroupId).Find(&groupAnalysis)
 		for _, analysis := range groupAnalysis {
 			groupsAnalysis = append(groupsAnalysis, analysis)
--- groups/model.go
+++ groups/model.go
@@ -4,35 +4,35 @@ import (
 	"time"
 )
 
-type Group struct {
-	GroupId            uint `gorm:"primary_key;AUTO_INCREMENT"`
-	Name               string
-	Desc               string
-	Code               string
-	CreatorId          uint
-	CreatedAt          time.Time
+type Group struct { // struct for groups
+	GroupId            uint                 `gorm:"primary_key;AUTO_INCREMENT"`
+	Name               string               // Name of group
+	Desc               string               // Description of group
+	Code               string               // Unique joining group
+	CreatorId          uint                 // Identifier of Creator
+	CreatedAt          time.Time            // time created
 	GroupTopicAnalyses []GroupTopicAnalysis `gorm:"foreignkey:GroupId"`
 	Members            []Member             `gorm:"foreignkey:GroupId"`
 }
 
-type Member struct {
-	MemberId uint `gorm:"primary_key;AUTO_INCREMENT"`
-	UserId   uint
-	GroupId  uint
-	JoinedAt time.Time
+type Member struct { // struct for members
+	MemberId uint      `gorm:"primary_key;AUTO_INCREMENT"`
+	UserId   uint      // Identifier of user
+	GroupId  uint      // Identifier of group
+	JoinedAt time.Time // time of membership
 	Banned   int
 }
 
-type GroupTopicAnalysis struct {
-	GTopId    uint `gorm:"primary_key;AUTO_INCREMENT"`
-	GroupId   uint
-	TopicId   uint
-	TopicName string
-	AvgResult float64
-	CreatedAt time.Time
+type GroupTopicAnalysis struct { // struct for group analysis
+	GTopId    uint      `gorm:"primary_key;AUTO_INCREMENT"`
+	GroupId   uint      // Identifier of group
+	TopicId   uint      // Identifier of topic
+	TopicName string    // Name of topic
+	AvgResult float64   // Average result
+	CreatedAt time.Time // Time Created
 }
 
-type Data struct {
+type Data struct { // struct to temporarily hold data
 	Groups             []Group
 	GroupTopicAnalysis []GroupTopicAnalysis
 }
