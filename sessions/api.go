66c2e032ed33337c8f120c46ce34092726d7d342
Aidan <aidan@fixedpointcode.com>
2019-04-07 15:25:53 +0100

    Added comments for session

--- sessions/api.go
+++ sessions/api.go
@@ -2,20 +2,32 @@ package sessions
 
 import (
 	"github.com/patrickmn/go-cache"
+	"log"
 	"time"
 )
 
-var c = cache.New(240*time.Minute, 40*time.Minute)
+var c = cache.New(240*time.Minute, 40*time.Minute) // Start cache
 
 func Get(token string) (interface{}, bool) {
+	// Get UserId using session token
 	value, found := c.Get(token)
 	if found == false {
-		panic("Invalid token")
+		log.Println("Invalid token")
 		return nil, false
 	}
 	return value, true
 }
 
 func Set(token string, UserId uint, expiry time.Duration) {
+	// Set session
 	c.Set(token, UserId, expiry)
+}
+
+func SetSession(UserId uint, bearerToken string) bool {
+	// Set session
+	if bearerToken == "" {
+		return false
+	}
+	Set("Bearer  "+bearerToken, UserId, 0)
+	return true
 }
